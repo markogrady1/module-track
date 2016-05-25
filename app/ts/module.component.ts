b38f2df4ad9344d68108206ee5e174e2df50c007
markogrady1 <markogrady.code@gmail.com>
2016-05-25 21:31:29 +0100

    add dynamic elements to form

--- app/ts/module.component.ts
+++ app/ts/module.component.ts
@@ -9,22 +9,32 @@ import {Component} from 'angular2/core';
 })
 
 export class ModuleComponent {
+   static MODULE_COUNT = 1;
+   modules= [0];
    moduleGrade1:number = 0;
    moduleWeight1:number = 0;
    output:number = 0;
    public calculatedAmount: number = 10;
     
-    validateGrade(field1, otherField) {
+    validateGrade(field1, otherField, index) {
+        console.log(index)
         field1.value = field1.value > 100 ? 100 : field1.value;
         field1.value = field1.value < 0 ? 0 : field1.value;
         this.moduleGrade1 = field1.value
         this.output = (this.moduleGrade1 * otherField.value) / 100
+        this.modules[index] = this.output
     }
     
-     validateWeight(field1, otherField) {
+     validateWeight(field1, otherField, index) {
+        console.log(index)
         field1.value = field1.value > 100 ? 100 : field1.value;
         field1.value = field1.value < 0 ? 0 : field1.value;
         this.moduleWeight1 = field1.value
         this.output = (this.moduleWeight1 * otherField.value) / 100
+        this.modules[index] = this.output
+    }
+    
+    addModule() {
+        this.modules.push(ModuleComponent.MODULE_COUNT++);
     }
 }
